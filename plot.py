df6a31b976c52efde5d8c9346aede1b5eb4eef7c
Nathan <nathancompbioakes@gmail.com>
2016-05-20 07:36:48 -0400

    test site

---
+++ plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import log10
+plt.style.use('ggplot')
+
+def main(argv):
+    df = pd.read_table(argv[1], header=None)
+    df = df[df > 0]
+    #ggplot(aes(x=range(len(df)), y=-1*df[[3]].applymap(log10), color=df[[1]]), data=df) + geom_point()
+    plt.scatter(range(len(df)), -1*df[[3]].applymap(log10), c=df[[1]])
+    plt.axhline(y=-log10(0.00000005), xmin=0, xmax=len(df), hold=None, color='r')
+    plt.xlabel('Chromosome')
+    plt.ylabel('-log10(p)')
+    plt.show()
+
+
+
+if __name__=="__main__":
+    main(sys.argv)
